8d0980a8a98449ea91bbde8cea21debc29ac9c09
airlay <airlay@gmail.com>
2019-04-25 13:49:27 -0700

    added more comment for further study

--- quickSelect.py
+++ quickSelect.py
@@ -2,5 +2,7 @@ def quickSelect(arr):
     # quickselect is a selection algorithm to find the kth smallest element in an unordered list.  closely related to
     # quicksort sorting algorithm.  Like qicksort it is efficient in practice and has good avg case perf, but has poor
     # worst case perf.
+    # https://www.geeksforgeeks.org/quickselect-algorithm/
+    # https://en.wikipedia.org/wiki/Quickselect
 
     pass
